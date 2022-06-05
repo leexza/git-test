9418676515fb7dbf8cbbf6f80f10344729637bb6
xza <xza@qq.com>
2022-06-05 20:45:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xza
+ * @create 2022-06-05 20:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
